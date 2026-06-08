8c179c2bbca851eab945dbd8d00fec8a20cacb4d
whyhilde <whyhilde@gmail.com>
2026-06-08 18:09:48 +0500

    feat(args): update args

--- src/args.rs
+++ src/args.rs
@@ -1,13 +1,15 @@
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-#[command(name = "rsget", about = "Download files")]
+#[command(version, about)]
 pub struct Args {
     pub url: String,
 
-    #[arg(short, long)]
+    /// Set output filename
+    #[arg(short, long, value_name = "FILE")]
     pub output: Option<String>,
 
+    /// Quiet mode
     #[arg(short, long, action = clap::ArgAction::SetTrue)]
     pub quiet: bool,
 }
